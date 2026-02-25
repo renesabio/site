10bfb38feffecdf9acb37664d1a2e6449b598c24
Renê Sábio <87675601+renesabio@users.noreply.github.com>
2026-02-24 21:05:27 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Renê | Portfólio & Inbound Marketing</title>
+    <link rel="stylesheet" href="style.css"> </head>
+<body>
+    <header>
+        <div class="container">
+            <h1>Renê</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Sobre Mim</a></li>
+                    <li><a href="portfolio.php">Portfólio</a></li>
+                    <li><a href="contato.php">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
