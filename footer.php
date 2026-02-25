93e69b26a929a8343ee870386f4f8bd00d3d407b
Renê Sábio <87675601+renesabio@users.noreply.github.com>
2026-02-24 21:10:34 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+    </main>
+    <footer>
+        <div class="container">
+            <p>&copy; <?php echo date("Y"); ?> Renê. Impulsionando resultados através de estratégias autênticas de Inbound.</p>
+        </div>
+    </footer>
+</body>
+</html>
